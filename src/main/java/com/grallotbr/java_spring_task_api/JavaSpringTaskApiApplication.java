9760b033d7ec4efda0136a169611d026db6d7297
Rafiere <rafapgbr@gmail.com>
2025-09-08 07:27:38 -0300

    feat: add initial java files

---
+++ src/main/java/com/grallotbr/java_spring_task_api/JavaSpringTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.grallotbr.java_spring_task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grallotbr/java_spring_task_api/JavaSpringTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grallotbr.java_spring_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
