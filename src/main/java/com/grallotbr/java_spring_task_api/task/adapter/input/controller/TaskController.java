b7dd806e22d3486af805111a1c80404c2ee37b1c
Rafiere <rafapgbr@gmail.com>
2025-09-09 10:00:02 -0300

    feat: add task api actions

---
+++ src/main/java/com/grallotbr/java_spring_task_api/task/adapter/input/controller/TaskController.java
@@ -0,0 +1,5 @@
+package com.grallotbr.java_spring_task_api.task.adapter.input.controller;
+
+public class TaskController {
+
+}
---
+++ src/main/java/com/grallotbr/java_spring_task_api/task/adapter/input/controller/request/CreateTaskRequest.java
@@ -0,0 +1,5 @@
+package com.grallotbr.java_spring_task_api.task.adapter.input.controller.request;
+
+public record CreateTaskRequest() {
+
+}
