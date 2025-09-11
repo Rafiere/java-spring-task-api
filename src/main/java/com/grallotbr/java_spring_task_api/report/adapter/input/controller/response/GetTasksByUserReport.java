281f7d8583fbcbbc7d8e9eeb5e05a76d5806f96a
Rafiere <rafapgbr@gmail.com>
2025-09-10 23:19:38 -0300

    feat: add get tasks by user report files

---
+++ src/main/java/com/grallotbr/java_spring_task_api/report/adapter/input/controller/response/GetTasksByUserReport.java
@@ -0,0 +1,5 @@
+package com.grallotbr.java_spring_task_api.report.adapter.input.controller.response;
+
+public record GetTasksByUserReport() {
+
+}
---
+++ src/main/java/com/grallotbr/java_spring_task_api/report/application/service/GetTasksByUserReportService.java
@@ -0,0 +1,7 @@
+ package com.grallotbr.java_spring_task_api.report.application.service;
+
+import com.grallotbr.java_spring_task_api.report.application.port.input.GetTasksByUserReportUseCase;
+
+public class GetTasksByUserReportService implements GetTasksByUserReportUseCase {
+
+}
