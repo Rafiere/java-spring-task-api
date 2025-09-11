0beceac61e3ce72e976ed783d7437135d98cdc76
Rafiere <rafapgbr@gmail.com>
2025-09-10 23:19:19 -0300

    feat: add get users files

---
+++ src/main/java/com/grallotbr/java_spring_task_api/user/application/port/in/GetUsersUseCase.java
@@ -0,0 +1,8 @@
+package com.grallotbr.java_spring_task_api.user.application.port.in;
+
+import com.grallotbr.java_spring_task_api.user.application.service.output.GetUsersOutput;
+
+public interface GetUsersUseCase {
+
+	GetUsersOutput execute();
+}
---
+++ src/main/java/com/grallotbr/java_spring_task_api/user/application/service/GetUsersService.java
@@ -0,0 +1,12 @@
+package com.grallotbr.java_spring_task_api.user.application.service;
+
+import com.grallotbr.java_spring_task_api.user.application.port.in.GetUsersUseCase;
+import com.grallotbr.java_spring_task_api.user.application.service.output.GetUsersOutput;
+
+public class GetUsersService implements GetUsersUseCase {
+
+	public GetUsersOutput execute() {
+
+		return null;
+	}
+}
