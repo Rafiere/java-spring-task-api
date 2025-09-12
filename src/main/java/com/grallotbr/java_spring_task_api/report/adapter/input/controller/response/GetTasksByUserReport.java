0bc5c4d03af90087d55a13f1a8e00b9a5a9a50a0
Rafiere <rafapgbr@gmail.com>
2025-09-12 09:54:44 -0300

    refactor: remove unused files

--- src/main/java/com/grallotbr/java_spring_task_api/report/adapter/input/controller/response/GetTasksByUserReport.java
+++
@@ -1,5 +0,0 @@
-package com.grallotbr.java_spring_task_api.report.adapter.input.controller.response;
-
-public record GetTasksByUserReport() {
-
-}
--- src/main/java/com/grallotbr/java_spring_task_api/report/application/service/GetTasksByUserReportService.java
+++
@@ -1,7 +0,0 @@
- package com.grallotbr.java_spring_task_api.report.application.service;
-
-import com.grallotbr.java_spring_task_api.report.application.port.input.GetTasksByUserReportUseCase;
-
-public class GetTasksByUserReportService implements GetTasksByUserReportUseCase {
-
-}
